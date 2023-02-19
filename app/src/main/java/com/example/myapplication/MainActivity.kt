eb0b1c2e22f3dce896b7a15b6ad1e491624d4a56
Mykola Shumeyko <85828245+NikShm@users.noreply.github.com>
2023-02-19 19:47:24 +0200

    First Lab

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val resultTextView: TextView = findViewById(R.id.textView)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            rollDice(diceImage, resultTextView)
+        }
+    }
+
+    private fun rollDice(diceImage: ImageView, resultTextView: TextView ) {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        resultTextView.text = diceRoll.toString()
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+    }
+
+    class Dice(val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
+}
